c1d09e7729eb7c526872545bdc8d39bf823258ad
Jane Yang <jane.yang@neo4j.com>
2022-11-29 13:54:46 +0100

    clean up

--- verification/setup.py
+++ verification/setup.py
@@ -17,8 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import os
 import setuptools
 
 # packages = setuptools.find_packages(exclude=["tests"])
